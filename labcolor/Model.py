b6fd8f425710e7788ba9b373f08be6cffcef703c
Girzmo2610 <hoangtuantu893@gmail.com>
2026-03-29 18:49:20 +0700

    Decompose the model from a single net layer into three separate layers.

--- labcolor/Model.py
+++ labcolor/Model.py
@@ -4,17 +4,22 @@ import torch.nn.functional as F
 class NeuralColor(nn.Module):
     def __init__(self):
         super().__init__()
-        self.net = nn.Sequential(
+        self.encoder = nn.Sequential(
             nn.Conv2d(4, 64, 3, padding=1), nn.ReLU(),
             nn.Conv2d(64, 128, 3, padding=1), nn.ReLU(),
-            nn.MaxPool2d(2),
-
+            nn.MaxPool2d(2)
+        )
+        self.middle = nn.Sequential(
             nn.Conv2d(128, 256, 3, padding=1), nn.ReLU(),
-            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False),
-
+            nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False)
+        )
+        self.decoder = nn.Sequential(
             nn.Conv2d(256, 128, 3, padding=1), nn.ReLU(),
-            nn.Conv2d(128, 2, 1)  # output ab
-            )
+            nn.Conv2d(128, 2, 1)
+        )
 
     def forward(self, x):
-        return self.net(x)
+        x = self.encoder(x)
+        x = self.middle(x)
+        x = self.decoder(x)
+        return x
